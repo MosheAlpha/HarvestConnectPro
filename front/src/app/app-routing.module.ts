578438c8a380ea11e3c66590f359821f3ab56b88
Bon1bon <bonim233@gmail.com>
2023-12-27 12:19:03 +0200

    new page routing added

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: WelcomePageComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'services', component: ServicesComponent },
+  // { path: 'blog', component: BlogComponent },
 ];
 
 @NgModule({
