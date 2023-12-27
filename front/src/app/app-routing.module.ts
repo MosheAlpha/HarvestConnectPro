995045b60ceaf6e7b561add2a2d6fc8bc3ed6694
Bon1bon <bonim233@gmail.com>
2023-12-27 12:48:38 +0200

    contact page added

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { VolunteeringComponent } from './pages/volunteering/volunteering.component';
 import { VolunteerDetailsComponent } from './pages/volunteer-details/volunteer-details.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: WelcomePageComponent },
   { path: 'volunteering', component: VolunteeringComponent },
   { path: 'volunteer-details', component: VolunteerDetailsComponent },
-  // { path: 'services', component: ServicesComponent },
-  // { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
