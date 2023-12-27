a37570f4684e583dff8dc1af5f3c264923325650
yoav101 <yoavaharoni101@gmail.com>
2023-12-27 19:56:42 +0200

    event component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     ContactComponent,
     LoginComponent,
     SignUpComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
